0c64870bcdff304ab59ea898320a0ab4e55c6b32
Marc Robert de Saint Vincent <marc.saintvincent@gmail.com>
2014-11-18 13:29:07 +0100

    Ajout de la classe PeerFile

---
+++ products/server/src/core/peer_file.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    peer_file.h
+    Created: 18 Nov 2014 1:27:48pm
+    Author:  Marc
+
+  ==============================================================================
+*/
+
+#ifndef PEER_FILE_H_INCLUDED
+#define PEER_FILE_H_INCLUDED
+
+
+
+
+
+#endif  // PEER_FILE_H_INCLUDED
