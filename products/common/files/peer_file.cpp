489bbf39a9eecd1ceeab005495005e2059fe6ccc
Adrien <adrien.tisseraud@gmail.com>
2014-11-24 13:30:33 +0100

    Amélioration des opérateurs d'égalité

--- products/common/files/peer_file.cpp
+++ products/common/files/peer_file.cpp
@@ -11,14 +11,46 @@
 #include "products/common/files/peer_file.h"
 
 PeerFile::PeerFile(const juce::File& file)
-: _file(file),
-  _md5(MD5(_file)) {}
+{
+  if (file.existsAsFile())
+  {
+    _file = file;
+    _filename = _file.getFileName();
+    _md5 = juce::MD5(_file).toHexString();
+    _size = _file.getSize();
+    _isLocal = true;
+  }
+  else
+  {
+    _file = juce::File::nonexistent;
+    _filename = juce::String::empty;
+    _md5 = juce::String::empty;
+    _size = 0;
+    _isLocal = false;
+  }
+}
 
-PeerFile::PeerFile(const PeerFile & newPeerFile)
-: _file(newPeerFile.getFile()),
-  _md5(MD5(_file)) {}
+PeerFile::PeerFile(juce::String filename, juce::String md5, int size)
+: _file(juce::File::nonexistent),
+  _filename(filename),
+  _md5(md5),
+  _size(size),
+  _isLocal(false) {}
 
-PeerFile::PeerFile() {}
+PeerFile::PeerFile(const PeerFile & otherFile)
+: _file(otherFile.getFile()),
+  _filename(otherFile.getFilename()),
+  _md5(otherFile.getMD5()),
+  _size(otherFile.getSize()),
+  _isLocal(otherFile.isLocal()),
+  _peers(otherFile.getPeersAddresses()) {}
+
+PeerFile::PeerFile()
+: _file(juce::File(juce::File::nonexistent)),
+  _filename(juce::String::empty),
+  _md5(juce::String::empty),
+  _size(0),
+  _isLocal(false) {}
 
 PeerFile::~PeerFile() {}
 
@@ -26,7 +58,9 @@ bool PeerFile::operator==(const PeerFile & otherFile) const
 {
   if (this->getMD5() != otherFile.getMD5())
     return false;
-  if (this->getFile().getFileName() != otherFile.getFile().getFileName())
+  if (this->getFilename() != otherFile.getFilename())
+    return false;
+  if (this->getSize() != otherFile.getSize())
     return false;
   return true;
 }
@@ -35,7 +69,22 @@ bool PeerFile::operator!=(const PeerFile & otherFile) const
 {
   if (this->getMD5() == otherFile.getMD5())
     return false;
-  if (this->getFile().getFileName() == otherFile.getFile().getFileName())
+  if (this->getFilename() == otherFile.getFilename())
+    return false;
+  if (this->getSize() == otherFile.getSize())
     return false;
   return true;
+}
+
+void PeerFile::addPeer(juce::IPAddress peer)
+{
+  if (!_peers.contains(peer))
+    _peers.add(peer);
+}
+
+void PeerFile::removePeer(juce::IPAddress peer)
+{
+  int index = _peers.indexOf(peer);
+  if (index >= 0)
+    _peers.remove(index);
 }
--- products/common/files/peer_file.h
+++ products/common/files/peer_file.h
@@ -18,11 +18,20 @@
 class PeerFile
 {
 public:
-  /// @brief Constructor
+  /// @brief Constructs a PeerFile from a local file, the MD5 hash and the size
+  /// are computed from the filesystem's info
+  /// @param {juce::File} file - Local file
   PeerFile(const juce::File& file);
   
-  /// @brief Copy constructor
-  PeerFile(const PeerFile &);
+  /// @brief Constructs a PeerFile from informations received over the network
+  /// @param {juce::String} filename - Name of the file
+  /// @param {juce::String} md5 - MD5 hash for this file
+  /// @param {int} size - Size in bytes
+  PeerFile(juce::String filename, juce::String md5, int size);
+  
+  /// @brief Constructs a PeerFile as a copy of another one
+  /// @param {PeerFile} otherFile - PeerFile that will be copied
+  PeerFile(const PeerFile & otherFile);
   
   /// @brief Destructor
   ~PeerFile();
@@ -30,8 +39,14 @@ public:
   /// @brief Get the associated file
   const juce::File& getFile() const { return _file; }
   
+  /// @brief Get the name of this file
+  const juce::String& getFilename() const { return _filename; }
+  
   /// @brief Get the MD5 message for this file
-  const juce::MD5& getMD5() const { return _md5; }
+  const juce::String& getMD5() const { return _md5; }
+  
+  /// @brief Get the size of the file
+  const int getSize() const { return _size; }
   
   /// @brief Test the equality between two PeerFile
   bool operator==(const PeerFile&) const;
@@ -39,15 +54,44 @@ public:
   /// @brief Test the inequality between two PeerFile
   bool operator!=(const PeerFile &) const;
   
+  /// @brief Test if this PeerFile is empty
+  const bool isEmpty() const { return (_size == 0) &&
+                                      (_filename == juce::String::empty) &&
+                                      (_file == juce::File::nonexistent) &&
+                                      (_md5 == juce::String::empty); }
+  
+  /// @brief True if this file is currently stored on this filesystem, meaning
+  /// that the getFile() method will give a valid file object
+  const bool isLocal() const { return _isLocal; }
+  
   /// @brief Returns an empty PeerFile
   static PeerFile emptyPeerFile () { return PeerFile(); }
+  
+  /// @brief Returns the list of Peers possessing this file
+  const juce::Array<IPAddress>& getPeersAddresses() const { return _peers; }
+  
+  /// @brief Add an IPAddress to the Peers list
+  void addPeer(juce::IPAddress peer);
+  
+  /// @brief Remove an IPAddress from the Peers list
+  void removePeer(juce::IPAddress peer);
 
 private:
   /// @brief Private default constructor
   PeerFile();
   
-  juce::File _file; ///< File associated
-  juce::MD5 _md5; ///< MD5 Hash for this file
+  /// @brief File associated
+  juce::File _file;
+  /// @brief Name of the file
+  juce::String _filename;
+  /// @brief MD5 Hash for this file
+  juce::String _md5;
+  /// @brief Size of the file
+  int _size;
+  /// @brief Define is the file is local or distant
+  bool _isLocal;
+  /// @brief List of Peers possessing this file
+  juce::Array<juce::IPAddress> _peers;
 };
 
 
